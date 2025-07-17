b4cc2741aa667dde64295f3ea473a3cba2d3db3f
Condor-OfSol <sarcher.ofsol@googlemail.com>
2025-07-17 11:29:26 +0100

    Added --verbose as a command lne argument. Argument may be added before OR after the query

--- main.py
+++ main.py
@@ -8,20 +8,35 @@ from google.genai import types
 
 def main():
     arguments = sys.argv
-    if len(arguments) < 1:
-        print("ERROR: no prompt provided...\nUSAGE: main.py \"YOUR PROMPT HERE\"")
+    print(f"{len(arguments)} number arguments provided")
+    verbose = False
+    query = ""
+
+    if len(arguments) < 2:
+        print("ERROR: no prompt provided...\nUSAGE: main.py \"YOUR PROMPT HERE\" [--OPTION]")
         exit(1)
+    elif len(arguments) == 3:
+        if arguments[2] == "--verbose":
+            verbose = True
+            query = arguments[1]
+        if arguments[1] == "--verbose":
+            verbose = True
+            query = arguments[2]
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     messages = [
-    types.Content(role="user", parts=[types.Part(text=arguments[1])]),
+    types.Content(role="user", parts=[types.Part(text=query)]),
     ]
     message = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
-    print(f"You asked: \"{arguments[1]}\"\n")
+    if verbose:
+        print("+++++++++++++++++++++ VERBOSE OUTPUT +++++++++++++++++++++")
+        print(f"User prompt: {arguments[1]}")
+        print(f"Prompt tokens: {message.usage_metadata.prompt_token_count}\nResponse tokens: {message.usage_metadata.candidates_token_count}")
+        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
     print(message.text)
-    print(f"Prompt tokens: {message.usage_metadata.prompt_token_count}\nResponse tokens: {message.usage_metadata.candidates_token_count}")
+        
 
 
 if __name__ == "__main__":
